99d7e4511000b6450894dc79408f17d41b2ef710
biaphan118 <biaphan2002@gmail.com>
2021-10-25 01:02:55 -0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,51 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //thông tin sinh viên
+		String ten = "Phan Van Nhat Huy";
+		String msv = "2160160" ;
+		String cmnd = "191920214" ;
+		String lop = "21CDTH41";
+		String hueic = "Cao đẳng Huế IC";
+		String tb = "10";
+				
+		
+		System.out.println(" Họ và tên " + ten);
+		System.out.println(" Mã sinh viên " + msv );
+		System.out.println(" Chứng minh nhân dân " + cmnd);
+		System.out.println(" lớp " + lop);
+		System.out.println(" Trường " + hueic);
+		System.out.println(" Điểm trung bình " + tb);
+		
+		// thông tin công nhân
+		String hoten = "Phan Van Nhat Huy";
+		String diachi = "23 Ly Nam De";
+		String sdt = "0788788975";
+		String hsl = "1 USD" ;
+		
+		System.out.println(" Họ tên " + hoten);
+		System.out.println(" Địa chỉ " + diachi );
+		System.out.println(" Số điện thoại " + sdt);
+		System.out.println(" Hệ số lương " + hsl);
+		
+		// thông tin 1 chiếc xe car
+		String xe = "Audi";
+		String hangxe = "Audi";
+		String gia = "100 USD";
+		String mau = "Hong" ;
+		
+		System.out.println(" Xe " + xe);
+		System.out.println(" Hãng " + hangxe );
+		System.out.println(" Giá " + gia);
+		System.out.println(" Màu " + mau);
+		
+		
+		
+
+
+	}
+
+}
